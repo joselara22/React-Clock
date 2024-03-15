cc415307f607a43d656c0659cd3757ac46e2a449
Jose Lara-Urbina <joselaraurb@gmail.com>
2024-03-15 19:55:45 -0400

    Added a todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Clock from "./Clock"
+import Todo from "./Todo"
 
 function App() {
 
   return (
-    <Clock />
+    <>
+      <Todo />
+      <Clock />
+    </>
   )
 }
 
---
+++ src/Todo.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+
+function Todo() {
+  const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+
+  const [tasks, setTasks] = useState(savedTasks);
+  const [task, setTask] = useState('');
+
+  // Update local storage when todo list changes
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks])
+
+  const onChange = (e) => {
+    setTask(e.target.value);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (task.trim() !== '') {
+      setTasks((prevState) => [...prevState, task]);
+      setTask('')
+    }
+  }
+
+  const onTaskClick = (index) => {
+    setTasks((prevState) => prevState.filter((task, i) => i !== index));
+  }
+
+  return (
+    <div className="todo-list">
+      <p>To Do</p>
+      <div className="input">
+        <form onSubmit={onSubmit}>
+          <input onChange={onChange} type="text" id="task" placeholder="Enter Task" value={task} />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+      {tasks.length > 0 ? <ul className="tasks">
+        {tasks.map((task, index) => (
+          <li onClick={() => onTaskClick(index)} key={index}>{task}</li>
+        ))}
+      </ul> : 'Take a break!'}
+      
+    </div>
+  )
+}
+
+export default Todo
